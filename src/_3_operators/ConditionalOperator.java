fe3fa730240d104b418613227f47c42a083bc8b1
akash waghmare <133653971+Akashwaghmare0507@users.noreply.github.com>
2025-02-17 15:19:04 +0530

    first commit

---
+++ src/_3_operators/ConditionalOperator.java
@@ -0,0 +1,10 @@
+package _3_operators;
+
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        System.out.println(
+                false ? "its a true value"
+                        : "its a false value"
+        );
+    }
+}
