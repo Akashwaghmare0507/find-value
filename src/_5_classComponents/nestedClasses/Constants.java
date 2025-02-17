05748c3eb122e367407b8cf32d9f54b90a6118eb
akash waghmare <133653971+Akashwaghmare0507@users.noreply.github.com>
2025-02-17 15:06:40 +0530

    first commit

---
+++ src/_5_classComponents/nestedClasses/Constants.java
@@ -0,0 +1,26 @@
+package _5_classComponents.nestedClasses;
+
+public class Constants {
+    static class MySql {
+        static class Dev {
+            public static final String USERNAME = "MysQL DEV user";
+            public static final String PASSWORD = "MySQL DEV pass";
+
+
+        }
+
+        static class QA {
+            public static final String USERNAME = "MysQL QA user";
+            public static final String PASSWORD = "MySQL QA pass";
+        }
+
+        static class Prod {
+            public static final String USERNAME = "MyaQL Prod user";
+            public static final String PASSWORD = "MySQL Prod pass";
+        }
+
+
+
+
+    }
+}
